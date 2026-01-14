a04ec83e7854d797a8e8f5e3aec37831fdc3b209
JulianMartin <julianser@hotmail.com.ar>
2026-01-14 01:23:01 -0300

    Se agrego cors al backend

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const express = require("express");
-require('dotenv').config();
+require("dotenv").config();
 const morgan = require("morgan");
 const connectDB = require("./src/config/database");
-const colorsRoutes = require("./src/routes/colors.routes")
-
-
-
+const colorsRoutes = require("./src/routes/colors.routes");
+const cors = require("cors");
 
 const app = express();
 
 connectDB();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`));
